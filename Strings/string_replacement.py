4efa27ac500c2cf8d36a616306a2d20d999f854d
Brendan Woo <woo.brendan@gmail.com>
2022-04-19 15:30:31 -0400

    complete first question for string replcaement

---
+++ Strings/string_replacement.py
@@ -0,0 +1,13 @@
+# Fix the below code into a proper string
+
+# journey = """Just a small tone girl
+# Leaving in a lonely whirl
+# She took the midnight tray going anywhere
+# Just a seedy boy
+# Bored and raised in South Detroit or something
+# He took the midnight tray going anywhere"""
+
+journey = "Just a small town girl \nLiving in a loney world.\nShe took the midnight train going anywhere \nJust a city boy \nBorn and raise in South Detroit \nHe took the midnight train going anywhere "
+
+
+print(journey)
