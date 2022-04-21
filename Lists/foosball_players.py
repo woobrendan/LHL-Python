f5b6328d2ef21367daddf70c1aecd0122e4d2ab5
Brendan Woo <woo.brendan@gmail.com>
2022-04-21 13:29:48 -0400

    create list folder with foosball player question setup

---
+++ Lists/foosball_players.py
@@ -0,0 +1,21 @@
+foosballers = [
+    "Mia",
+    "Retta",
+    "Augustine",
+    "Jin",
+    "Waylon",
+    "Alphonso",
+    "Sage",
+    "Hubert",
+    "Raymon",
+    "Rebecca",
+    "Monty",
+    "Glen",
+    "Christi",
+    "Patrice",
+    "Craig",
+    "Dexter",
+    "Wally",
+    "Ian",
+    "Pat"
+]
