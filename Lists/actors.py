c786e2ebfd75314864d43d1c09ddb74895799b5a
Brendan Woo <woo.brendan@gmail.com>
2022-04-22 14:47:33 -0400

    add iteration over lists questions

---
+++ Lists/actors.py
@@ -0,0 +1,10 @@
+actors = ["Nathan Fillion", "Gina Torres", "Alan Tudyk", "Morena Baccarin",
+          "Adam Baldwin", "Jewel Staite", "Sean Maher", "Summer Glau", "Ron Glass"]
+
+roles = ["Captaim Malcolm Reynolds", "Zoe Washburn", "Hoban Washburn", "Inara Serra",
+         "Jayne Cobb", "Kaylee Fyre", "Dr. Simon Tam", "River Tam", "Derrial Book"]
+
+i = 0
+while i < len(actors):
+    print(f"{actors[i]} as {roles[i]}")
+    i += 1
